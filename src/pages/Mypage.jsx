39529a18464fbc6772d75a7c4f78a94fc529ca8f
hyeonsu <hskim2265@ewhain.net>
2024-09-19 14:00:49 +0900

    ✨ : 마이페이지 퍼블리싱 1차 완료

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,14 +1,110 @@
 import SideBar from "../components/SideBar";
 import * as S from "./Mypage.style";
 import profile from "../assets/profile.svg";
+import Button from "../components/Botton";
+import { useState } from "react";
 
 const Mypage = () => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const onClickIsEdit = () => {
+    setIsEdit(true);
+  };
+
+  const onClickSave = () => {
+    setIsEdit(false);
+  };
   return (
     <>
       <SideBar />
 
       <S.Wrapper>
-        <img src={profile} />
+        <S.ProfileWrp>
+          <img src={profile} style={{ width: "4.5rem" }} />
+          <S.Welcom>닉네임님 환영합니다!</S.Welcom>
+        </S.ProfileWrp>
+        <S.Line />
+        <S.GreyText>회원 정보</S.GreyText>
+
+        <S.UserWrp>
+          <S.User1>
+            <S.UserTitle>이름</S.UserTitle>
+            <S.UserInfo>김근주</S.UserInfo>
+          </S.User1>
+          <S.User2>
+            <S.UserTitle>닉네임</S.UserTitle>
+            {isEdit ? (
+              <>
+                <S.UserInfoInput />
+              </>
+            ) : (
+              <S.UserInfo>근주근주</S.UserInfo>
+            )}
+          </S.User2>
+        </S.UserWrp>
+
+        {/*  */}
+        <S.UserWrp>
+          <S.User1>
+            <S.UserTitle>아이디</S.UserTitle>
+            <S.UserInfo>asdfs</S.UserInfo>
+          </S.User1>
+          <S.User1>
+            <S.UserTitle1>생년월일</S.UserTitle1>
+            {isEdit ? (
+              <>
+                <S.UserInfoInput />
+              </>
+            ) : (
+              <S.UserInfo>1950. 10. 10</S.UserInfo>
+            )}
+          </S.User1>
+        </S.UserWrp>
+
+        <S.Line />
+        <S.GreyText>연락 정보</S.GreyText>
+        <S.UserWrp>
+          <S.UserCall>
+            <S.UserTitle2>핸드폰</S.UserTitle2>
+            {isEdit ? (
+              <S.UserInfoInputLong />
+            ) : (
+              <S.UserInfo2>010-1111-2222</S.UserInfo2>
+            )}
+          </S.UserCall>
+        </S.UserWrp>
+
+        <S.UserWrp>
+          <S.UserCall>
+            <S.UserTitle2>이메일</S.UserTitle2>
+            {isEdit ? (
+              <S.UserInfoInputLong />
+            ) : (
+              <S.UserInfo2>skljd@naver.com</S.UserInfo2>
+            )}
+          </S.UserCall>
+        </S.UserWrp>
+
+        <S.UserWrp>
+          <S.UserCall>
+            <S.UserTitle2>비상 연락망</S.UserTitle2>
+            {isEdit ? (
+              <S.UserInfoInputLong />
+            ) : (
+              <S.UserInfo2>010-1234-1234 (딸)</S.UserInfo2>
+            )}
+          </S.UserCall>
+        </S.UserWrp>
+        {isEdit ? (
+          <Button type={"L"} onClick={onClickSave}>
+            저장하기
+          </Button>
+        ) : (
+          <Button type={"L"} onClick={onClickIsEdit}>
+            수정하기
+          </Button>
+        )}
+        <div style={{ height: "3rem" }} />
       </S.Wrapper>
     </>
   );
--- src/pages/Mypage.style.js
+++ src/pages/Mypage.style.js
@@ -4,7 +4,163 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 7rem;
+  padding-top: 4rem;
   left: 16.15rem;
   margin-left: 16.15rem;
+`;
+
+export const Welcom = styled.div`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-left: 1.75rem;
+`;
+
+export const ProfileWrp = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  margin-bottom: 1.7rem;
+`;
+
+export const Line = styled.div`
+  width: 39rem;
+  height: 0.05rem;
+  background: #d9d9d9;
+  margin-bottom: 1rem;
+`;
+
+export const GreyText = styled.div`
+  color: #a2a1a1;
+  text-align: center;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  width: 39rem;
+  text-align: start;
+  margin-bottom: 2.5rem;
+`;
+
+export const UserWrp = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 38rem;
+  margin-bottom: 2.5rem;
+`;
+
+export const UserTitle = styled.div`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-right: 3.25rem;
+  width: 3.65rem;
+`;
+export const UserTitle1 = styled.div`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-right: 3.25rem;
+  width: 4.85rem;
+`;
+
+export const UserInfo = styled.div`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  width: 10rem;
+`;
+
+export const UserInfoInput = styled.input`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  width: 10rem;
+  height: 3rem;
+  flex-shrink: 0;
+  outline: none;
+  box-sizing: border-box;
+  padding: 1rem 1rem;
+
+  border-radius: 0.35rem;
+  border: 1px solid #d3d3d3;
+  background: #fff;
+`;
+
+export const UserInfoInputLong = styled.input`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  width: 18.45rem;
+  height: 3rem;
+  flex-shrink: 0;
+  outline: none;
+  box-sizing: border-box;
+  padding: 1rem 1rem;
+
+  border-radius: 0.35rem;
+  border: 1px solid #d3d3d3;
+  background: #fff;
+`;
+
+export const User1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const User2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  width: 18.65rem;
+  align-items: center;
+`;
+
+export const UserTitle2 = styled.div`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-right: 3.25rem;
+  width: 7rem;
+`;
+
+export const UserInfo2 = styled.div`
+  color: #000;
+  font-family: var(--korean);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+export const UserCall = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
 `;
