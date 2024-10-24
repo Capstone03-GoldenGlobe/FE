8933d963362e0625a5e76f7ef6475eeeb551296e
hyeonsu <hskim2265@ewhain.net>
2024-10-24 15:48:54 +0900

    ⚡️ : 회원가입 api 연결

--- src/api/signup.js
+++ src/api/signup.js
@@ -4,21 +4,19 @@ export const SignupApi = async (
   name,
   birth,
   cellphone,
-  email,
   password,
   nickname,
   profile,
   gender
 ) => {
   try {
-    const res = await client.post("/auth/signup", {
+    const res = await client.post("auth/signup", {
       name: String(name),
-      email: String(email),
-      password: String(password),
       birth: String(birth),
       cellphone: String(cellphone),
+      password: String(password),
       nickname: String(nickname),
-      profile: String(profile),
+      profile: "",
       gender: String(gender),
     });
 
